6406251db89540cc544694f2bb38c66a6cacf876
yogaekat <yogaekat@gmail.com>
2025-12-12 07:56:08 +0800

    update layout

---
+++ assets/js/swiper.js
@@ -0,0 +1,45 @@
+var swiper = new Swiper(".mySwiperShop", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+        },
+    },
+});
+
+var swiper = new Swiper(".mySwiperTeam", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    // pagination: {
+    //     el: ".swiper-pagination",
+    //     clickable: true,
+    // },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+        },
+    },
+});
